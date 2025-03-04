c7f9df3881915d857a5237ac49c6a5e61691ba8d
SerkanOkut <serkan.okt56@gmail.com>
2025-03-04 17:10:32 +0300

    hata deneme

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,27 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if (!isBarking || clock<0 || clock>23 ) {
+            return  false;
+        }
+        return clock < 8 || clock >= 20;
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        return (firstAge >= 13 && firstAge <= 19) ||
+                (secondAge >= 13 && secondAge <= 19) ||
+                (thirdAge >= 13 && thirdAge <= 19);
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+
     }
 
     public static double area(double width, double height) {
-       
+
     }
 
     public static double area(double radius) {
-       
+
     }
 }
