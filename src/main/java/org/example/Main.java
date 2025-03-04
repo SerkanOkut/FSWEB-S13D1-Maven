210e8570863fee313e12390e86c1ce9781b56b5e
SerkanOkut <serkan.okt56@gmail.com>
2025-03-04 17:51:18 +0300

    Sprint 13 gün 1 projesi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,20 +13,38 @@ public class Main {
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-        return (firstAge >= 13 && firstAge <= 19) ||
+        if ((firstAge >= 13 && firstAge <= 19) ||
                 (secondAge >= 13 && secondAge <= 19) ||
-                (thirdAge >= 13 && thirdAge <= 19);
+                (thirdAge >= 13 && thirdAge <= 19)){
+            return true;
+        }
+        return  false;
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
+      if(isSummer) {
+          if (temp >= 25 && temp <=45) {
+              return true;
+          }
+      } else  {
+              if (temp>=25 && temp<=35){
+                  return true;
+              }
+          }
+            return false;
+          }
 
-    }
 
     public static double area(double width, double height) {
-
+        if (width < 0 || height < 0) {
+            return -1;
+        }
+        return width*height ;
     }
-
     public static double area(double radius) {
-
+    if (radius<0) {
+        return -1;
+    }
+    return radius * radius*Math.PI;
     }
 }
